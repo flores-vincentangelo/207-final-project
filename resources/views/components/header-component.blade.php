80904ad77b39e4e586bb25c09b5a042e21ea3690
Vincent <vincentflores88@gmail.com>
2025-04-30 23:01:09 +0800

    added alpine

--- resources/views/components/header-component.blade.php
+++ resources/views/components/header-component.blade.php
@@ -10,7 +10,7 @@
             </form>
         </div>
     </div>
-    
+
     <a href="/home">
         <div class="flex flex-col items-center">
             <div>
@@ -24,8 +24,15 @@
         </div>
     </a>
 
-
     <div class="search-container flex flex-row items-center">
+        <div>
+            <svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 3H16V1H0V3Z" fill="#000000" />
+                <path d="M2 7H14V5H2V7Z" fill="#000000" />
+                <path d="M4 11H12V9H4V11Z" fill="#000000" />
+                <path d="M10 15H6V13H10V15Z" fill="#000000" />
+            </svg>
+        </div>
         <form action="/home/" method="GET">
             <input type="text" id="query" name="query" placeholder="search">
             <button class="form-button">Search</button>
