459e78a5215b92d8ae1234202f4909a4ccf5b7ca
Vincent <vincentflores88@gmail.com>
2025-04-30 18:21:46 +0800

    middleware

--- routes/web.php
+++ routes/web.php
@@ -8,21 +8,18 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
-    $products = [];
-    $cart = null;
-    $cart_products = null;
-    if(Auth::user()) {
-        $products = Product::all();
-        $cart = Auth::user()->cart()->get();
-        if ($cart->isEmpty()) {
-            $cart = (new CartController)->createCart();
-        }
 
-        $cart_products = $cart[0]->products()->get();
-        $product2 = $cart[0]->products()->where('product_id', 6)->get();
+    $user = Auth::user();
+    $products = Product::all();
+    $cart = Auth::user()->cart()->get();
+    if ($cart->isEmpty()) {
+        $cart = (new CartController)->createCart();
     }
-    return view('home', ['products' => $products, 'cart' => $cart[0], 'cart_products' => $cart_products, 'product2' => $product2]);
-});
+
+    $cart_products = $cart[0]->products()->get();
+    
+    return view('home', ['user'=> $user, 'products' => $products, 'cart' => $cart[0], 'cart_products' => $cart_products]);
+})->middleware('auth');
 
 Route::get('/login', function () {
     return view('login');
@@ -39,3 +36,13 @@ Route::post('/register', [UserController::class, 'register']);
 
 Route::post('/add-to-cart/{product}', [CartController::class, 'addToCart']);
 Route::put('/update-cart/{product}', [CartController::class, 'updateCart']);
+
+Route::get('/checkout', function() {
+    $cart = Auth::user()->cart()->get();
+    $cart_products= $cart[0]->products()->get();
+    if($cart_products->isEmpty()) {
+        return redirect('/');
+    }
+
+    return view('checkout', ["cartproducts" => $cart_products]);
+})->middleware('auth');
