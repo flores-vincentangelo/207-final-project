4fda5806231be193a280fb29998cd59732c2cc11
Vincent <vincentflores88@gmail.com>
2025-04-30 16:55:45 +0800

    product list

--- resources/views/components/product-list.blade.php
+++ resources/views/components/product-list.blade.php
@@ -4,15 +4,19 @@
             <div class="product-list-entry">
                 <img
                     class="image-styling" 
-                    src="{{ $product['image'] }}" alt="">
+                    src="{{ $product->image }}" alt="">
             </div>
             <div class="flex flex-col">
-                <div class="product-list-entry">{{$product['name']}}</div>
-                <div class="product-list-entry .product-list-description bg-green">{{$product['description']}}</div>
-                <div class="product-list-entry">{{$product['price']}}</div>
+                <div class="product-list-entry">{{$product->name}}</div>
+                <div class="product-list-entry .product-list-description bg-green">{{$product->description}}</div>
+                <div class="product-list-entry">{{$product->price}}</div>
             </div>
             <div>
-                add to cart
+                <form action="/add-to-cart/{{ $product->id }}" method="POST">
+                    @csrf
+                    <input type="number" name="quantity" id="quantity" value=1>
+                    <button>Add to cart</button>
+                </form>
             </div>
         </div>
 
