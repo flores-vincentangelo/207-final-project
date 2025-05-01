710badfe3276d0ec6b1d128626cdfe5c2a389f34
Vincent <vincentflores88@gmail.com>
2025-05-01 11:06:24 +0800

    cart updates

--- resources/views/components/cart-component.blade.php
+++ resources/views/components/cart-component.blade.php
@@ -1,43 +1,55 @@
 <div :class="{'translate-x-full': !cartOpen}" class="shadowing cart-container bg-white z-10 fixed inset-y-0 right-0 top-0 transition eas-in-out duration-200 ">
-    cart
-    <div>
-        <button @click="cartOpen = ! cartOpen">close</button>
-    </div>
-    @php
-        $totalCost = 0
-    @endphp
-    <table>
-        <tr>
-            <th></th>
-            <th>Product Name</th>
-            <th>quantity</th>
-        </tr>
+    <div class="flex flex-col items-center">
+        <div class="close-container flex flex-row items-center">
+            <button @click="cartOpen = ! cartOpen">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024">
+                    <path fill="#000000" d="M195.2 195.2a64 64 0 0 1 90.496 0L512 421.504 738.304 195.2a64 64 0 0 1 90.496 90.496L602.496 512 828.8 738.304a64 64 0 0 1-90.496 90.496L512 602.496 285.696 828.8a64 64 0 0 1-90.496-90.496L421.504 512 195.2 285.696a64 64 0 0 1 0-90.496z"/>
+                </svg>
+            </button>
+        </div>
+        <div class="flex flex-row justify-center items-center w-full">
+            <h2>
+                Cart
+            </h2>
+        </div>
 
-        @foreach ($cartproducts as $product)
-                @php
-                    $totalCost += $product->price * $product->pivot->quantity
-                @endphp
-                <tr>
-                    <td>
-                        <img class="image-styling" src="{{ $product->image }}" alt="">
-                    </td>
-                    <td>{{$product->name}}</td>
-                    <td>
-                        <form action="/update-cart/{{ $product->id }}" method="POST">
-                            @csrf
-                            @method('PUT')
-                            <input type="number" name="quantity" id="quantity" value={{ $product->pivot->quantity }}>
-                            <button>Update cart</button>
-                        </form>
-                    </td>
-                </tr>
-        @endforeach
-    </table>
-    <div>
-        {{ $totalCost }}
+        <div>
+            @php
+            $totalCost = 0
+        @endphp
+        <table>
+            <tr>
+                <th></th>
+                <th>Product Name</th>
+                <th>quantity</th>
+            </tr>
+    
+            @foreach ($cartproducts as $product)
+                    @php
+                        $totalCost += $product->price * $product->pivot->quantity
+                    @endphp
+                    <tr>
+                        <td>
+                            <img class="image-styling" src="{{ $product->image }}" alt="">
+                        </td>
+                        <td>{{$product->name}}</td>
+                        <td>
+                            <form action="/update-cart/{{ $product->id }}" method="POST">
+                                @csrf
+                                @method('PUT')
+                                <input type="number" name="quantity" id="quantity" value={{ $product->pivot->quantity }}>
+                                <button>Update cart</button>
+                            </form>
+                        </td>
+                    </tr>
+            @endforeach
+        </table>
+        <div>
+            {{ $totalCost }}
+        </div>
+        <div>
+            <a href="/checkout">Checkout</a>
+        </div>
+        </div>
     </div>
-    <div>
-        <a href="/checkout">Checkout</a>
-    </div>
-
 </div>
